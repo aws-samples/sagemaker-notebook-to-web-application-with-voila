54c63bc450e0c3ccd482e18e5114a49fcdbac66e
Jahed <jahez@amazon.fr>
2023-10-26 16:08:00 +0200

    Add cdk-nag for security checks

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+import aws_cdk as cdk
+from aws_cdk import Aspects
+from cdk_nag import AwsSolutionsChecks
 
 from cdk.cdk_stack import CdkStack
 
 
-app = core.App()
+app = cdk.App()
 CdkStack(app, "voila-app-sharing-dashboard")
 
+Aspects.of(app).add(AwsSolutionsChecks())
 app.synth()
